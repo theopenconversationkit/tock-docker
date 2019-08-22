947a57e38ba77f23612d3e89949921db212fab50
Julien Buret <jburet@voyages-sncf.com>
2019-08-22 12:01:47 +0200

    improves release script
    Signed-off-by: Julien Buret <jburet@voyages-sncf.com>

--- etc/releaseAndPush.sh
+++ etc/releaseAndPush.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-echo "Release target: \$1 -> version \$2 -> major"
+echo "Release target: $1 -> version / $2 -> major"
 mvn release:clean release:prepare release:perform -Ddeploy "-Dtock=$1" "-Dmajor=$2" -Dlatest=latest
