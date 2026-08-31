d9c4398e92a1630741cf009203365b3598a281e3
Mohamed ASSOUKTI <mohamed.assoukti@partnre.com>
2026-08-31 17:53:09 +0200

    [DERCBOT-2070] Langfuse V4

--- scripts/pgvector/init.sql
+++ scripts/pgvector/init.sql
@@ -1,8 +1,36 @@
 CREATE EXTENSION IF NOT EXISTS vector;
 
-CREATE TABLE IF NOT EXISTS embeddings (
-  id SERIAL PRIMARY KEY,
-  embedding vector,
-  text text,
-  created_at timestamptz DEFAULT now()
+CREATE TABLE IF NOT EXISTS langchain_pg_collection (
+    uuid UUID NOT NULL,
+    name VARCHAR NOT NULL,
+    cmetadata JSON,
+    CONSTRAINT langchain_pg_collection_pkey PRIMARY KEY (uuid),
+    CONSTRAINT langchain_pg_collection_name_key UNIQUE (name)
 );
+
+CREATE TABLE IF NOT EXISTS langchain_pg_embedding (
+    id VARCHAR NOT NULL,
+    collection_id UUID,
+    embedding VECTOR,
+    document VARCHAR,
+    cmetadata JSONB,
+    CONSTRAINT langchain_pg_embedding_pkey PRIMARY KEY (id),
+    CONSTRAINT langchain_pg_embedding_collection_id_fkey
+    FOREIGN KEY (collection_id)
+    REFERENCES langchain_pg_collection (uuid)
+    ON DELETE CASCADE
+);
+
+CREATE INDEX ix_cmetadata_gin
+    ON langchain_pg_embedding
+    USING gin (cmetadata jsonb_path_ops);
+
+CREATE EXTENSION IF NOT EXISTS unaccent;
+
+ALTER TABLE langchain_pg_embedding
+    ADD COLUMN fts_vector tsvector
+        GENERATED ALWAYS AS (
+            to_tsvector('french', COALESCE(document, ''))
+            ) STORED;
+
+CREATE INDEX idx_fts_vector ON langchain_pg_embedding USING GIN(fts_vector);
